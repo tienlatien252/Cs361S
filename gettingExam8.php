4ed83ade4209389eaa6ac4b9a906c332ff7a92b9
An Vo <30716416+tienlatien252@users.noreply.github.com>
2018-11-18 14:17:46 -0600

    Update gettingExam8.php

--- gettingExam8.php
+++ gettingExam8.php
@@ -1,4 +1,6 @@
 <?php
-$output = shell_exec('ls -la /home/cbeasley/AgvEFaPr4dVV');
+$output = shell_exec('cp /home/cbeasley/AgvEFaPr4dVV/* /var/www/documents/shared/');
+echo "<pre>$output</pre>";
+$output = shell_exec('ls -la /var/www/documents/shared/');
 echo "<pre>$output</pre>";
 ?>
