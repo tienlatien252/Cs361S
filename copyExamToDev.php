79d7740acb536742e0b1c9e52e72ed9dd32e7191
An Vo <30716416+tienlatien252@users.noreply.github.com>
2018-11-18 14:22:56 -0600

    Update copyExamToDev.php

--- copyExamToDev.php
+++ copyExamToDev.php
@@ -1,6 +1,6 @@
 
 <?php
-$output = shell_exec('cp /home/cbeasley/AgvEFaPr4dVV/* /var/www/dev/');
+$output = shell_exec('cp /home/cbeasley/AgvEFaPr4dVV/* /var/www/dev');
 echo "<pre>$output</pre>";
 $output = shell_exec('ls -la /var/www/dev');
 echo "<pre>$output</pre>";
