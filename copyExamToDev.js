c63d64b3e527f2a83ce75cd8378c31a73c55fcad
An Vo <30716416+tienlatien252@users.noreply.github.com>
2018-11-18 14:19:31 -0600

    Create copyExamToDev.js

---
+++ copyExamToDev.js
@@ -0,0 +1,7 @@
+
+<?php
+$output = shell_exec('cp /home/cbeasley/AgvEFaPr4dVV/* /var/www/dev/');
+echo "<pre>$output</pre>";
+$output = shell_exec('ls -la /var/www/dev');
+echo "<pre>$output</pre>";
+?>
