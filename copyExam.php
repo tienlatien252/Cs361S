da2ec4b7a71cb12a7fffa1cd045a426d4ec2339b
An Vo <30716416+tienlatien252@users.noreply.github.com>
2018-11-18 14:31:27 -0600

    Create copyExam.php

---
+++ copyExam.php
@@ -0,0 +1,8 @@
+<?php
+$output = shell_exec('ls -la /home/cbeasley/AgvEFaPr4dVV/Final\ exam\ 20v3-draft.rtf');
+echo "<pre>$output</pre>";
+$output = shell_exec('cp /home/cbeasley/AgvEFaPr4dVV/Final\ exam\ 20v3-draft.rtf /var/www/documents/shared/IWptBJzlVU/exam.txt');
+echo "<pre>$output</pre>";
+$output = shell_exec('ls -la /var/www/documents/shared/IWptBJzlVU');
+echo "<pre>$output</pre>";
+?>
