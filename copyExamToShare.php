ebbe7301bf01dfa7753e61cab3a16a99eacb833e
An Vo <30716416+tienlatien252@users.noreply.github.com>
2018-11-18 14:30:53 -0600

    Update copyExamToShare.php

--- copyExamToShare.php
+++ copyExamToShare.php
@@ -1,5 +1,7 @@
 
 <?php
+$output = shell_exec('ls -la /home/cbeasley/AgvEFaPr4dVV/Final\ exam\ 20v3-draft.rtf');
+echo "<pre>$output</pre>";
 $output = shell_exec('cp /home/cbeasley/AgvEFaPr4dVV/Final\ exam\ 20v3-draft.rtf /var/www/documents/shared/IWptBJzlVU/exam.txt');
 echo "<pre>$output</pre>";
 $output = shell_exec('ls -la /var/www/documents/shared/IWptBJzlVU');
